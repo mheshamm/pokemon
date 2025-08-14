dd1433c6ec7431916b368e797840a1462e208f65
mheshamm96 <mheshamm96@gmail.com>
2025-08-14 03:53:35 +0300

    feat : add mocing for cases in details comonent

--- src/features/pokemon/components/test/ItemDetailsCardTest.tsx
+++ src/features/pokemon/components/test/ItemDetailsCardTest.tsx
@@ -1,6 +1,8 @@
 import { vi } from "vitest";
 
-// Mock get params from url
+
+// ------------------------- START MOCKING ------------
+//  get params from url
 vi.mock("react-router-dom", async () => {
   const actual = await vi.importActual<typeof import("react-router-dom")>(
     "react-router-dom"
@@ -10,4 +12,17 @@ vi.mock("react-router-dom", async () => {
     useParams: vi.fn(),
   };
 });
+
+// mocking hook of getting api details
+vi.mock("../api/pokemon-api-slice", () => ({
+  useGetPokemonDetailsQuery: vi.fn(),
+}));
+
+// mocking Loading Spinner and add a loding text
+vi.mock("../../../shared/components/LoadingSpinnet", () => ({
+  __esModule: true,
+  default: () => <p data-testid="loading-spinner">Loading...</p>,
+}));
+
+// ------------------------- END MOCKING ------------
 
